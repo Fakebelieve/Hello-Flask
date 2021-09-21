18370bc413600afcfa1352288f9e9402a1fa7dd0
KeygunM <>
2021-09-21 04:21:35 +0000

    added contact back

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ def hello_world():
 	x = random.randint(1,10)
 	return '<h1>Hello, World'+str(x)+'!</h1>'
 
+@app.route('/contact')
+def contact():
+	num = ""
+	for x in range(0,10):
+		num = num + str(random.randint(0,10))
+	return '<i>Phone number:'+num+'!</i>'
 
 app.run(host='0.0.0.0', port=8080)
